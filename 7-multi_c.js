6b2b760262923fb6d4c70ecae82ef7bf304fd0dd
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-09-09 06:07:06 +0000

    edited

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,9 +3,16 @@ const num = parseInt(firstArg);
 if (!isNaN(num)) {
   let output = '';
   for (let i = 0; i < num; i++) {
-    output += 'C is fun\n';
+    output += 'C is fun';
   }
-  console.log(output.trim());
+  console.log(output);
 } else {
   console.log('Missing number of occurrences');
 }
+
+/* Where x is the first argument of the script
+If the first argument can’t be converted to an integer, print “Missing number of occurrences”
+You must use console.log(...) to print all output
+You are not allowed to use var
+You can use only two console.log
+You must use a loop (while, for, etc.) */
