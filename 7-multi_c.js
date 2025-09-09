369131f21ff554e8d15292dc764395e51464689b
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-09-09 05:56:18 +0000

    Script to write 'C is fun'

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const firstArg = process.argv[2];
+const num = Number(firstArg);
+
+if(!isNaN(num)) {
+  for(let i=0; i< num; i++){
+    console.log("C is fun");
+  }
+}else {
+  console.log("Missing number of occurrences");
+}
