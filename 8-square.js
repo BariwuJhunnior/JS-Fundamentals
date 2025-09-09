a7f0cf23b6591a0e1f615cf4cdedc8447560943f
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-09-09 06:31:50 +0000

    while loop for 'for' loop

--- 8-square.js
+++ 8-square.js
@@ -1,21 +1,18 @@
 const first_Arg = process.argv[2];
 const num = parseInt(first_Arg);
 
-if(!isNaN(num)){
-  let row = "";
+if (!isNaN(num)) {
+  let row = '';
   let i = 0;
-
-  while(i < num){
+  while (i < num) {
     row += 'X';
     i++;
   }
-
   let j = 0;
-  while(j < num){
+  while (j < num) {
     console.log(row);
     j++;
   }
-}
-else{
-  console.log("Missing size");
+} else {
+  console.log('Missing size');
 }
